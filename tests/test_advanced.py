445c84c5294815e8cde7aab7865e7c59ad8e6cdc
Thomas Milliman <thomas.milliman@unh.edu>
2019-01-24 16:13:40 -0500

    add some tests

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -4,12 +4,17 @@ from .context import sirpy2
 
 import unittest
 
-
 class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
-    def test_thoughts(self):
-        self.assertIsNone(.hmm())
+    def test_extractsirhead(self):
+        file = "tests/msfa-a-NAm07-181-185.sir"
+        sir = sirpy2.loadsir(file)
+        sh = sirpy2.extractsirhead(sir[1])
+        self.assertEqual(sh['region'], 'NAm')
+        self.assertEqual(sh['year'], 2007)
+        self.assertEqual(sh['nsx'], 1890)
+        self.assertEqual(sh['nsy'], 1150)        
 
 
 if __name__ == '__main__':
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,8 +8,12 @@ import unittest
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
-    def test_absolute_truth_and_meaning(self):
-        assert True
+    def test_fn2dt(self):
+        fname = "msfa-a-NAm07-181-185.sir"
+        result = sirpy2.fn2dt(fname)
+        self.assertEqual(result.year, 2007)
+        self.assertEqual(result.month, 7)
+        self.assertEqual(result.day, 2)
 
 
 if __name__ == '__main__':
