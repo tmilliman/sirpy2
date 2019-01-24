6349b7519f975ad24a21b1badd8c09925b06f79b
Thomas Milliman <thomas.milliman@unh.edu>
2019-01-24 16:23:49 -0500

    convert tests to pytest

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from .context import sirpy2
+import sirpy2
 
-import unittest
-
-class AdvancedTestSuite(unittest.TestCase):
-    """Advanced test cases."""
-
-    def test_extractsirhead(self):
+def test_extractsirhead():
         file = "tests/msfa-a-NAm07-181-185.sir"
         sir = sirpy2.loadsir(file)
         sh = sirpy2.extractsirhead(sir[1])
-        self.assertEqual(sh['region'], 'NAm')
-        self.assertEqual(sh['year'], 2007)
-        self.assertEqual(sh['nsx'], 1890)
-        self.assertEqual(sh['nsy'], 1150)        
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert sh['region'] == 'NAm'
+        assert sh['year'] == 2007
+        assert sh['nsx'] == 1890
+        assert sh['nsy'] == 1150
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from .context import sirpy2
+import sirpy2
 
-import unittest
+def test_fn2dt():
+    fname = 'msfa-V-NAm07-181-185.sir'
+    result = sirpy2.fn2dt(fname)
+    assert result.year == 2007
+    assert result.month == 7
+    assert result.day == 2
 
-
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
-
-    def test_fn2dt(self):
-        fname = "msfa-a-NAm07-181-185.sir"
-        result = sirpy2.fn2dt(fname)
-        self.assertEqual(result.year, 2007)
-        self.assertEqual(result.month, 7)
-        self.assertEqual(result.day, 2)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    
